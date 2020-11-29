ae79ad0261005974454d42bf8190f3a4cb855332
bwbonanno <bradfordbonanno@gmail.com>
2020-11-29 03:43:08 +0000

    Git commands return short commit sha

--- backend/git/git.go
+++ backend/git/git.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
-	"github.com/mitchellh/go-homedir"
 )
 
 var privateKeyFile = "/root/.ssh/id_rsa"
+var repositoryLocationBase = "/root/DeployedRepos/"
 
 func getPublicKeys() (*ssh.PublicKeys, error) {
 	if _, err := os.Stat(privateKeyFile); err != nil {
@@ -26,42 +26,63 @@ func getPublicKeys() (*ssh.PublicKeys, error) {
 
 // CloneRepoToLocation will clone the repository to the specified path on
 // the server
-func CloneRepoToLocation(repository string, location string) error {
-	location, _ = homedir.Expand(location)
+func CloneRepoToLocation(repository string, location string) (string, error) {
+	location = repositoryLocationBase + location
 	publicKeys, err := getPublicKeys()
 	if err != nil {
-		return err
+		return "", err
 	}
 	_, err = git.PlainClone(location, false, &git.CloneOptions{
 		Auth:     publicKeys,
 		URL:      repository,
 		Progress: os.Stdout,
 	})
-	return err
+	repo, err := git.PlainOpen(location)
+	if err != nil {
+		log.Fatalf("Failed to open git path: %s\n", err.Error())
+		return "", err
+	}
+	return getHeadHash(repo)
 }
 
 // PullRepoAtLocation pulls latest changes from git repo
-func PullRepoAtLocation(location string) error {
+func PullRepoAtLocation(location string) (string, error) {
+	location = repositoryLocationBase + location
 	repository, err := git.PlainOpen(location)
 	if err != nil {
 		log.Fatalf("Failed to open git path: %s\n", err.Error())
-		return err
+		return "", err
 	}
 
 	worktree, err := repository.Worktree()
 	if err != nil {
 		log.Fatalf("Failed to get worktree for repo: %s\n", err.Error())
-		return err
+		return "", err
 	}
 
 	publicKeys, err := getPublicKeys()
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	return worktree.Pull(&git.PullOptions{
+	err = worktree.Pull(&git.PullOptions{
 		RemoteName: "origin",
 		Auth:       publicKeys,
 		Progress:   os.Stdout,
 	})
+
+	if err != nil {
+		return "", err
+	}
+	return getHeadHash(repository)
+}
+
+func getHeadHash(repository *git.Repository) (string, error) {
+	head, err := repository.Head()
+	if err != nil {
+		return "", err
+	}
+
+	hash := head.Hash()
+	return hash.String()[:8], nil
 }
