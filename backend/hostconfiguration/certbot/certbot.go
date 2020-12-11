dd6faacd64442c263e6a5a9d625c4a0c41d6a71e
bwbonanno <bradfordbonanno@gmail.com>
2020-12-11 04:59:23 +0000

    Certbot args to force SSL

--- backend/hostconfiguration/certbot/certbot.go
+++ backend/hostconfiguration/certbot/certbot.go
@@ -7,7 +7,7 @@ import (
 
 // UpdateCertificates runs certbot to support the domains specified
 func UpdateCertificates(domains []hostconfiguration.DomainConfiguration) error {
-	certbotArgs := []string{"--nginx", "--non-interactive", "--agree-tos", "--expand"}
+	certbotArgs := []string{"--nginx", "--non-interactive", "--agree-tos", "--expand", "--redirect", "--hsts"}
 	for _, domainConfig := range domains {
 		certbotArgs = append(certbotArgs, "-d")
 		certbotArgs = append(certbotArgs, domainConfig.Domain)
