231bebd9b8ca2a9549dd0a673dec5108e97ca1da
bwbonanno <bradfordbonanno@gmail.com>
2020-12-12 04:02:49 +0000

    Use go routine for update network config

--- backend/routes/updateNetworkConfigs.go
+++ backend/routes/updateNetworkConfigs.go
@@ -1,17 +1,11 @@
 package routes
 
 import (
-	"deployed/utils"
-	"log"
 	"net/http"
 )
 
 // UpdateNetworkConfigs updates the Nginx and certificate configurations
 func UpdateNetworkConfigs(w http.ResponseWriter, r *http.Request) {
-	err := updateNetworkConfigs()
-	if err != nil {
-		log.Printf("Failed to update network configs")
-		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
-	}
+	go updateNetworkConfigs()
 	w.WriteHeader(http.StatusOK)
 }
